8ac6f14156ed25d5f230ba945799e333099f93d7
Piotr <piotrsiwiec.lfc@outlook.com>
2024-03-02 18:38:31 +0100

    commit

---
+++ java_lista_3.java
@@ -0,0 +1,11 @@
+public class lista_3 {
+    // ZADANIE 1
+
+
+    public static void main(String[] args) {
+        // ZADANIE 1
+        System.out.println("ZADANIE 1");
+
+
+    }
+}
---
+++ kotlin_lista_3.kt
@@ -0,0 +1,26 @@
+fun numberOfOccurrences(input: String): Map<String, Int> {
+    val wordOccurrences = mutableMapOf<String, Int>()
+
+    // Podziel zdanie na słowa, uwzględniając polskie znaki
+    val words = input.split("\\s+".toRegex())
+
+    for (word in words) {
+        // Usuń interpunkcję i zamień na małe litery
+        val cleanedWord = word.replace("[^a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]".toRegex(), "").toLowerCase()
+
+        // Zlicz wystąpienia słów
+        val count = wordOccurrences.getOrDefault(cleanedWord, 0)
+        wordOccurrences[cleanedWord] = count + 1
+    }
+
+    return wordOccurrences
+}
+
+fun main() {
+    val input: String = "To jest przykładowe zdanie. To zdanie jest tylko przykładowe."
+    val result = numberOfOccurrences(input)
+
+    for ((word, count) in result) {
+        println("Word: '$word' occurs $count times.")
+    }
+}
