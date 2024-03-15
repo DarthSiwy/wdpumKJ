8ece3fdda9847d8d22e14adceabdc6c6077d5334
DarthSiwy <piotrsiwiec.lfc@outlook.com>
2024-03-15 23:23:40 +0100

    commit

--- lista_4.kt
+++ lista_4.kt
@@ -27,13 +27,39 @@ fun srt(input: List<String>): List<Pair<Char, List<String>>> {
         .map { Pair(it.first, it.second.sorted().filter{ it.length % 2 == 0 }) }
 }
 
+fun perm(input: List<Int>): List<List<Int>> {
+    if (input.size == 1) return listOf(input)
+    val perms = mutableListOf<List<Int>>()
+    val toInsert = input[0]
+    for (perm in perm(input.drop(1))) {
+        for (i in 0..perm.size) {
+            val newPerm = perm.toMutableList()
+            newPerm.add(i, toInsert)
+            perms.add(newPerm)
+        }
+    }
+    return perms
+}
+
+fun check(N: Int, List: List<Int>): Int {
+    for (pre in List.windowed(3)) {
+        println(pre.find{it % 2  == 0})
+
+
+    }
+
+    return -1
+}
+
 fun main() {
     val results = listOf(
         lambda("Ala",4),
         suma(listOf(1, -4, 12, 0, -3, 29, -150)),
         countElements(listOf( listOf("a", "b", "c"), listOf("c", "d", "f"), listOf("d", "f", "g"))),
         evenPositiveSquare(listOf(1, 2, 3, 5, -6, -1, -1, 2, 3)),
-        srt(listOf("cherry", "blueberry", "citrus", "apple", "apricot", "banana", "coconut"))
+        srt(listOf("cherry", "blueberry", "citrus", "apple", "apricot", "banana", "coconut")),
+        perm(listOf(1, 2, 3)),
+        check(2, listOf(1, 2, 3, 4, 5, 6))
     )
 
     results.forEachIndexed { index, result ->
